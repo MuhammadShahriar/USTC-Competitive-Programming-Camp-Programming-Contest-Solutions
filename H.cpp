f67e7c866b5fc59ccef3d775ac1e7e6a4b46a3c7
Muhammad Shahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2023-01-10 22:43:05 +0600

    Solution of Problem H

---
+++ H.cpp
@@ -0,0 +1,101 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+int cnt[30];
+
+int main()
+{
+    string s, ans;
+    cin >> s;
+    int n = s.size();
+
+    for ( int i = 0; i < n; i++ ) cnt[s[i]-'A']++;
+
+    int d = 0;
+    int ch = -1;
+
+    for ( int i = 0; i < 26; i++ ) {
+        if ( cnt[i] % 2 == 1 ) {
+            d++;
+            ch = i;
+        }
+    }
+
+    if ( d > 1 ) {
+        cout << "NO SOLUTION" << endl;
+        return 0;
+    }
+
+    for ( int i = 0; i < 26; i++ ) {
+        int lim = cnt[i] / 2;
+        for ( int j = 0; j < lim; j++ ) {
+            ans += (i + 'A');
+        }
+    }
+
+    if( ch != -1 ) ans += ch + 'A';
+
+    for ( int i = 25; i >= 0; i-- ) {
+        int lim = cnt[i] / 2;
+        for ( int j = 0; j < lim; j++ ) {
+            ans += (i + 'A');
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
