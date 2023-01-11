1522c7e542a5d82c57d0184fc1511258e502ec98
Muhammad Shahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2023-01-11 23:36:50 +0600

    Solution of Problem K

---
+++ K.cpp
@@ -0,0 +1,125 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+const int mx = 2e7+123;
+
+bitset<mx> is_prime;
+vector<int> prime;
+int cnt[mx];
+
+void primeGen ( int n )
+{
+    for ( int i = 3; i <= n; i += 2 ) is_prime[i] = 1;
+
+    int sq = sqrt ( n );
+    for ( int i = 3; i <= sq; i += 2 ) {
+        if ( is_prime[i] == 1 ) {
+            for ( int j = i*i; j <= n; j += ( i + i ) ) {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[2] = 1;
+    for ( int i = 3; i <= n; i += 2 ) {
+        if ( is_prime[i] == 1 ) prime.push_back ( i );
+    }
+}
+
+int main()
+{
+    optimize();
+
+    int lim = 2e7;
+    int mxLim = 1e7;
+    primeGen(lim);
+
+    int limX = 3200;
+    int limY = 60;
+
+    for ( int x = 1; x <= limX; x++ ) {
+        for ( int y = 1; y <= limY; y++ ) {
+            int val = ( x * x ) + ( y * y * y * y );
+            if ( val < mxLim && is_prime[val] ) {
+                cnt[val] = 1;
+            }
+        }
+    }
+
+    for ( int i = 1; i <= mxLim; i++ ) cnt[i] += cnt[i-1];
+
+    int t;
+    cin >> t;
+    while ( t-- ) {
+        int n;
+        cin >> n;
+        cout << cnt[n] << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
